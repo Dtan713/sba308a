fc25b76426a7c76a814d2aa558dd933bd6e827dc
Dtan713 <s.donaldtan@gmail.com>
2024-06-18 12:16:25 -0400

    adding little more

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 
 async function getDogimages() {
   const response = await fetch(
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMWJqP3gln_06nqC_HE2Ec00WzPHhlGcP_SQ&s'    
+    'https://api.thedogapi.com/v1/images/search?limit=10'    
   );
 
   const data = await response.json();
+  console.log(data)
+    data.forEach((element) => {
+    console.log(element);
+    
+  });
 
-console.log(data);
 }
 
 getDogimages();
 
-function createImagesTag(){
-    const img = document.createElement['img']
+function createImagesTag(src){
+    const img = document.createElement("img");
+    log.setAtttibutes("src", src);
 }
+
+const img = new Image();
+img.src = "https://cdn2.thedogapi.com/images/B1d5me547_1280.jpg', width: 554, height: 546" //
 
 
